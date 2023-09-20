7c0cce2f2b4f771b361327787f08447dbd094e89
Gul Jain <z5430560@ad.unsw.edu.au>
2023-09-20 13:22:51 +1000

    stub for function adminQuizList

---
+++ src/quiz.js
@@ -0,0 +1,10 @@
+function adminUserDetails (authUserId) {
+    return { user: {
+        userId: 1,
+        name: 'Hayden Smith',
+        email: 'hayden.smith@unsw.edu.au',
+        numSuccessfulLogins: 3,
+        numFailedPasswordsSinceLastLogin: 1,
+        }
+   }             
+}
