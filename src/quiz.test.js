dd41142f3018ef59d69ae749ca5752f4c19779c1
Jessica <z5444474@ad.unsw.edu.au>
2023-09-27 17:55:23 +1000

    Deleted second quiz remove test

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -65,38 +65,6 @@ describe('Testing adminQuizInfo', () => {
   });
 });
 
-describe ('Testing adminQuizRemove', () => {
-    test('Correct input', () => {
-        const NewUser = adminAuthRegister('jess@hotmail.com', '123456ab', 'Jess', 'Tran');
-        const QuizId = adminQuizCreate(NewUser, 'Jess', 'description');
-        expect(adminQuizRemove(NewUser, QuizId)).toStrictEqual({});
-    });
-    
-    test ('Empty input', () => {
-        expect(adminQuizRemove('', '')).toStrictEqual({ error: expect.any(String) });
-    });
-    
-    test ('Invalid AuthUserId', () => {
-        const NewUser = adminAuthRegister('jess@hotmail.com', '123456ab', 'Jess', 'Tran');
-        const QuizId = adminQuizCreate(NewUser, 'Jess', 'description');
-        expect(adminQuizRemove('abc', QuizId)).toStrictEqual({ error: expect.any(String) });
-    });
-    
-    test ('Invalid QuizId', () => {
-        const NewUser = adminAuthRegister('jess@hotmail.com', '123456ab', 'Jess', 'Tran');
-        const QuizId = adminQuizCreate(NewUser, 'Jess', 'description');
-        expect(adminQuizRemove(NewUser, 'abc')).toStrictEqual({ error: expect.any(String) });
-    });
-    
-    test ('QuizId does not refer to a quiz that this user owns', () => {
-        const JessUser = adminAuthRegister('jess@hotmail.com', '123456ab', 'Jess', 'Tran');
-        const AdamUser = adminAuthRegister('adam@hotmail.com', 'ab123456', 'Adam', 'Lee');
-        const JessQuizId = adminQuizCreate(NewUser, 'Jess', 'description');
-        const AdamQuizId = adminQuizCreate(NewUser, 'Jess', 'description');
-        expect(adminQuizRemove(JessUser, AdamQuizId)).toStrictEqual({ error: expect.any(String) });
-    });    
-});
-
 describe('Testing adminQuizCreate', () => {
     test('Test Valid Auth User ID', () => {
       const authUserId = adminAuthRegister('email@gamil.com', 'password', 'Saarthak', 'Sinha');
