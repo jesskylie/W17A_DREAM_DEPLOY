3e3f2f3a57de0cb77be6e347369f5088deb45ebf
Gul Jain <z5430560@ad.unsw.edu.au>
2023-09-27 18:56:28 +1000

    Correcting changes in AuthAdminRegister in test file auth.test.js

--- src/auth.test.js
+++ src/auth.test.js
@@ -31,42 +31,46 @@ describe ('Testing adminAuthLogin', () => {
 });
 
 describe ('Testing adminAuthRegister', () => {
+
+    test ('Valid: Admin registered' , () => {
+        expect(adminAuthRegister('any@gmail.com', '1234PassWord', 'gul','Jain').toStrictEqual({}));
+    });
     test ('Invalid: NameFirst connot contain numbers', () => {
-        expect(adminAuthLogin('any@gmail.com', '1234Password', 'gul01', 'Surname')).toStrictEqual({error: except.any(string)});        
+        expect(adminAuthRegister('any@gmail.com', '1234Password', 'gul01', 'Surname')).toStrictEqual({error: except.any(string)});        
     });
 
     test ('Invalid: NameLast connot contain numbers', () => {
-        expect(adminAuthLogin('any@gmail.com', '1234Password', 'gul', 'surname09')).toStrictEqual({error: except.any(string)});        
+        expect(adminAuthRegister('any@gmail.com', '1234Password', 'gul', 'surname09')).toStrictEqual({error: except.any(string)});        
     });
 
     test ('Invalid: NameFirst cannot contain symbols', () => {
-        expect(adminAuthLogin('any@gmail.com', '1234Password', 'gul@%', 'surname')).toStrictEqual({error: except.any(string)});  
+        expect(adminAuthRegister('any@gmail.com', '1234Password', 'gul@%', 'surname')).toStrictEqual({error: except.any(string)});  
     });
 
     test ('Invalid: NameLast cannot contain symbols', () => {
-        expect(adminAuthLogin('any@gmail.com', '1234Password', 'gul', 'surname%%')).toStrictEqual({error: except.any(string)});  
+        expect(adminAuthRegister('any@gmail.com', '1234Password', 'gul', 'surname%%')).toStrictEqual({error: except.any(string)});  
     });
 
     test ('Invalid: NameFirst must have more than 2 and less than 20 characters', () => {
-        expect(adminAuthLogin('any@gmail.com', '1234Password', 'a', 'surname')).toStrictEqual({error: except.any(string)});  
+        expect(adminAuthRegister('any@gmail.com', '1234Password', 'a', 'surname')).toStrictEqual({error: except.any(string)});  
     });
 
     test ('Invalid: NameLast must have more than 2  and less than 20 characters', () => {
-        expect(adminAuthLogin('any@gmail.com', '1234Password', 'name', 'r')).toStrictEqual({error: except.any(string)});  
+        expect(adminAuthRegister('any@gmail.com', '1234Password', 'name', 'r')).toStrictEqual({error: except.any(string)});  
     });
 
     test ('Invalid: NameFirst must have more than 2  and less than 20 characters', () => {
-        expect(adminAuthLogin('any@gmail.com', '1234Password', 'nameshouldbelessthantwenty', 'r')).toStrictEqual({error: except.any(string)});  
+        expect(adminAuthRegister('any@gmail.com', '1234Password', 'nameshouldbelessthantwenty', 'r')).toStrictEqual({error: except.any(string)});  
     });
 
     test ('Invalid: NameLast must have more than 2  and less than 20 characters', () => {
-        expect(adminAuthLogin('any@gmail.com', '1234Password', 'name', 'surnameshouldbelessthantwenty')).toStrictEqual({error: except.any(string)});  
+        expect(adminAuthRegister('any@gmail.com', '1234Password', 'name', 'surnameshouldbelessthantwenty')).toStrictEqual({error: except.any(string)});  
     });
     test ('Invalid: Password is less than 8 characters', () => {
-        expect(adminAuthLogin('any@gmail.com', '1Pass', 'name', 'surname')).toStrictEqual({error: except.any(string)});  
+        expect(adminAuthRegister('any@gmail.com', '1Pass', 'name', 'surname')).toStrictEqual({error: except.any(string)});  
     });
 
     test('Invalid: Password must contain at least one number and at least one letter', () => {
-        expect(adminAuthLogin('any@gmail.com', 'invalidpassword', 'name', 'surname')).toStrictEqual({error: except.any(string)});  
+        expect(adminAuthRegister('any@gmail.com', 'invalidpassword', 'name', 'surname')).toStrictEqual({error: except.any(string)});  
     });
 });
