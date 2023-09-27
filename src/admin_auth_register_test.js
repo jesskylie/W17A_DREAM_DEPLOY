5ecb44d894c065d3873c6a23236c09a9488c7302
Gul Jain <z5430560@ad.unsw.edu.au>
2023-09-27 18:34:01 +1000

    gul added adminAuthRegister test in auth.test.js file.

--- src/admin_auth_register_test.js
+++ src/admin_auth_register_test.js
@@ -1,34 +1,4 @@
-import {
-    adminAuthRegister,
-    adminAuthLogin,
-    adminUserDetails,
-  } from './auth.js';
-  
-import { clear } from './other.js';
-
-beforeEach(() => {
-    clear();
-  });
-  
-
-describe ('Testing adminAuthLogin', () => {
-    test ('email address does not exist', () => {
-        expect(adminAuthLogin('jess@hotmail.com', '123456AB')).toStrictEqual( {error: expect.any(String)} );
-    });
-    
-    test ('password is incorrect for given email', () => {
-        const NewUser = adminAuthRegister('jess@hotmail.com', '123456ab', 'Jess', 'Tran');
-        const login = adminAuthLogin('jess@hotmail.com', '');
-        expect(login).toStrictEqual( {error: expect.any(String)} );
-    });
-    
-    test('correct input', () => {
-        const NewUser = adminAuthRegister('jess@hotmail.com', '123456ab', 'Jess', 'Tran');
-        const login = adminAuthLogin('jess@hotmail.com', '123456ab');
-        expect(login).toStrictEqual( { authUserId: expect.any(Number)} );
-    });
-    
-});
+import { adminAuthRegister } from './auth.js';
 
 describe ('Testing adminAuthRegister', () => {
     test ('Invalid: NameFirst connot contain numbers', () => {
