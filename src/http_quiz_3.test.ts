1f0d8117c80ef4b0b302d8e4473ef62d17300ae2
Jessica <z5444474@ad.unsw.edu.au>
2023-11-05 23:37:35 +1100

    Finished implementing tests for  /v2/admin/quiz

--- src/http_quiz_3.test.ts
+++ src/http_quiz_3.test.ts
@@ -11,12 +11,38 @@ describe('Testing /v2/admin/quiz', () => {
     const result = requestAdminRegister('jess@hotmail.com', '12345abced', 'Jess', 'Tran');
     expect(requestAdminQuizCreateV2(result.body.token, 'New Quiz', 'Quiz description')).toStrictEqual({ quizId: expect.any(Number) });
   });
-  
-	test('Token is empty or invalid ', () => {
+
+  test('Token is empty or invalid - error 401 ', () => {
     requestClear();
     requestAdminRegister('jess@hotmail.com', '12345abced', 'Jess', 'Tran');
     expect(() => requestAdminQuizCreateV2('', 'New Quiz', 'Quiz description')).toThrow(HTTPError[401]);
   });
-	
-	
+
+  test('Name contains invalid characters - error 400 ', () => {
+    requestClear();
+    const result = requestAdminRegister('jess@hotmail.com', '12345abced', 'Jess', 'Tran');
+    expect(() => requestAdminQuizCreateV2(result.body.token, '$$$$', 'Quiz description')).toThrow(HTTPError[400]);
+  });
+
+  test('Name is less than not between 3 - 30 characters long - error 400 ', () => {
+    requestClear();
+    const result = requestAdminRegister('jess@hotmail.com', '12345abced', 'Jess', 'Tran');
+    expect(() => requestAdminQuizCreateV2(result.body.token, 'A', 'Quiz description')).toThrow(HTTPError[400]);
+    expect(() =>
+      requestAdminQuizCreateV2(result.body.token, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'Quiz description')).toThrow(HTTPError[400]);
+  });
+
+  test('Name is already used by the current logged in user for another quiz ', () => {
+    requestClear();
+    const result = requestAdminRegister('jess@hotmail.com', '12345abced', 'Jess', 'Tran');
+    requestAdminQuizCreateV2(result.body.token, 'QuizOne', 'Quiz description');
+    expect(() => requestAdminQuizCreateV2(result.body.token, 'QuizOne', 'description')).toThrow(HTTPError[400]);
+  });
+
+  test('Name is already used by the current logged in user for another quiz ', () => {
+    requestClear();
+    const result = requestAdminRegister('jess@hotmail.com', '12345abced', 'Jess', 'Tran');
+    requestAdminQuizCreateV2(result.body.token, 'QuizOne', 'Quiz description');
+    expect(() => requestAdminQuizCreateV2(result.body.token, 'QuizOne', 'description')).toThrow(HTTPError[400]);
+  });
 });
